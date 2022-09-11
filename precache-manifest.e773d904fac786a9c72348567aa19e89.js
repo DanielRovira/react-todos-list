f4969efa041b7d573970b298da3bf1d9483f523b
DanielRovira <danielroviratorres@gmail.com>
2022-09-11 10:45:13 +0200

    Updates

---
+++ precache-manifest.e773d904fac786a9c72348567aa19e89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a1db47db421543c5fef",
+    "url": "/projeto-react/static/css/main.0ba14b58.chunk.css"
+  },
+  {
+    "revision": "0a1db47db421543c5fef",
+    "url": "/projeto-react/static/js/main.9321d37e.chunk.js"
+  },
+  {
+    "revision": "ee5e8f94ff0801833bea",
+    "url": "/projeto-react/static/js/runtime~main.d89d00b6.js"
+  },
+  {
+    "revision": "3e82256cef15be5b23db",
+    "url": "/projeto-react/static/js/2.b31d900e.chunk.js"
+  },
+  {
+    "revision": "687fb524e2597233c263e6fb5bad3e37",
+    "url": "/projeto-react/index.html"
+  }
+];
